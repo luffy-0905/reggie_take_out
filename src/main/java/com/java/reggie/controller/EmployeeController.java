4fbbb63b76096bf1514996300128eddf4d4024a3
luffy <luffy@qq.com>
2022-08-10 01:33:51 +0800

    third commit

--- src/main/java/com/java/reggie/controller/EmployeeController.java
+++ src/main/java/com/java/reggie/controller/EmployeeController.java
@@ -1,17 +1,16 @@
 package com.java.reggie.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.java.reggie.common.Contants;
 import com.java.reggie.common.R;
 import com.java.reggie.entity.Employee;
 import com.java.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
@@ -80,5 +79,36 @@ public class EmployeeController {
 
         employeeService.save(employee);
         return R.success("新增员工成功");
+    }
+
+    /**
+     * 分页查询
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page,int pageSize,String name){
+        log.info("page = {},pageSize = {},name = {}",page,pageSize,name);
+        //构造分页构造器
+        Page pageInfo = new Page(page,pageSize);
+
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper();
+        //过滤条件（模糊查询）
+        queryWrapper.like(StringUtils.isNotEmpty(name),Employee::getName,name);
+        //排序条件
+        queryWrapper.orderByDesc(Employee::getUpdateTime);
+        employeeService.page(pageInfo,queryWrapper);
+        return R.success(pageInfo);
+    }
+
+    /**
+     * 禁用员工账号
+     * @return
+     */
+    @PutMapping
+    public R<String> enableOrDisableEmployee(){
+        return null;
     }
 }
